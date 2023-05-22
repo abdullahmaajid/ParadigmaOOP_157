d599109d6e112024e971c297b50b0325b938fcab
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-05-22 15:56:58 +0700

    fix

--- mainA/ibu.h
+++ mainA/ibu.h
@@ -1,28 +1,31 @@
 #pragma once
 #ifndef IBU_H
 #define IBU_H
+#include <iostream>;
+#include "anak.h"
+#include <vector>
+using namespace std;
 
 class ibu {
 public:
 	string nama;
-	vector<anak> daftar_anak;
+	vector<anak*> daftar_anak;
 
-	ibu(string pNama) : nama(pNama) {
+	ibu(string pNama) :nama(pNama) {
 		cout << "Ibu \"" << nama << "\" ada \n";
 	}
 	~ibu() {
-		cout << "Ibu \"" << nama << "\" tidak ada \n";
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
 	}
-	void tambahAanak(anak*);
+	void tambahAnak(anak*);
 	void cetakAnak();
 };
-
-void ibu::tambahAanak(anak* pAnak) {
+void ibu::tambahAnak(anak* pAnak) {
 	daftar_anak.push_back(pAnak);
 }
 void ibu::cetakAnak() {
 	cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
-	for (auto& : daftar_anak) {
+	for (auto& a : daftar_anak) {
 		cout << a->nama << "\n";
 	}
 	cout << endl;
