b194aab73157eaf9a30cc26785594e490d503d85
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-05-22 15:24:18 +0700

    mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai) {   // definisi  }

--- ParadigmaOOP_157/ParadigmaOOP_157.cpp
+++ ParadigmaOOP_157/ParadigmaOOP_157.cpp
@@ -9,6 +9,18 @@ private:
 	float nilai;
 
 public:
-	mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai)
+	mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai) {
 		// definisi
+	}
+
+	~mahasiswa() {
+		cout << "Id = " << id << endl;
+		cout << "Nama = " << nama << endl;
+		cout << "Nilai = " << nilai << endl;
+	}
 };
+
+int main() {
+	mahasiswa mhs(12, "Asroni", 90.5);
+
+}
